dae0c0f4e8682d1804a52b2624c67a68ce64f03c
yuhanwww <ywang70@smith.edu>
2024-11-05 14:44:14 -0500

    Add tag into the project list

--- src/components/Project/projects.jsx
+++ src/components/Project/projects.jsx
@@ -8,21 +8,32 @@ import makersmap from '../../assets/projects/makersmap/makersmap.png';
 import exploreCSR from '../../assets/projects/exploreCSR/exploreCSR.png';
 
 const Data = [
+    // {
+    //     id: 0,
+    //     image: phyllotaxis,
+    //     alt: "Trust in AI",
+    //     title: "Honors Thesis Project",
+    //     description: "Honor Thesis Project on factors that influence trust in AI in higher education",
+    //     category:["HCI","research"],
+    // },
+
     {
         id: 1,
         image: phyllotaxis,
-        alt: "Trust in AI",
-        title: "TDA on Phyllotaxis",
-        description: "Honor Thesis Project on factors that influence trust in AI in higher education",
+        alt: "Artwork",
+        title: "Artworks on Human-Technology Relationship",
+        description: "Three Artworks based on p5.js & arduino from Digital Multimedia course",
+        category:["Art","HCI"],
     },
 
     {
-        id: 2,
-        image: phyllotaxis,
-        alt: "Guerilla Girl Artwork",
-        title: "Guerilla AI",
-        description: "A Computer Vision-Assisted Investigation inspired by Guerilla Girls",
-    },
+      id: 2,
+      image: phyllotaxis,
+      alt: "Guerilla Girl Artwork",
+      title: "Guerilla AI",
+      description: "A Computer Vision-Assisted Investigation inspired by Guerilla Girls",
+      category:["Machine Learning","Art History"],
+  },
 
     {
         id: 3,
@@ -30,6 +41,7 @@ const Data = [
         alt: "Phyllotaxis Hexagonal Heatmap",
         title: "TDA on Phyllotaxis",
         description: "A Topological Data Analysis study on Phyllotaxis in Prof. Christophe Golé's lab",
+        category:["Visualization","Research"],
     },
 
     {
@@ -38,6 +50,7 @@ const Data = [
         alt: "Artwork Matrix",
         title: "Mystery Art Recognizer",
         description: "Final Project for MTH 353 Deep Learning Seminar & ARH 212 Ancient Cities & Sanctuaries",
+        category:["Machine Learning","Art History"],
     },
 
     {
@@ -46,6 +59,7 @@ const Data = [
         alt: "Bat Fungus Wordcloud",
         title: "Break Through Tech AI",
         description: "Application of Machine Learning on Bat Fungus Detection and Plant Speciman Classification",
+        category:["Machine Learning"],
     },
 
     {
@@ -54,6 +68,7 @@ const Data = [
         alt: "Smith Makers Map",
         title: "Smith Makers Map",
         description: "An Interactive Making Resource Map for Smith College",
+        category:["Website Design","Intern"],
     },
 
     {
@@ -62,6 +77,7 @@ const Data = [
         alt: "phyllotaxis hexagonal heatmap",
         title: "Brown exploreCSR",
         description:"SVG creations from the exploreCSR program @Brown Spring 2023",
+        category:["Design","Research"],
     },
 
     {
@@ -70,6 +86,7 @@ const Data = [
         alt: "Valet Bike App Homepage",
         title: "Valet Bike",
         description:"A Bike Rental Website with Ruby on Rails by team Bugz for CSC 223 Software EGR",
+        category:["Website Design","Software Engineering"],
     },
 
     {
@@ -78,20 +95,26 @@ const Data = [
         alt: "Health App Wireframe",
         title: "Campus Mental Health App",
         description:"A Mental Health App Prototype for EGR 100 Topics in Health",
+        category:["Prototype","HCI"],
     },
 
 ];
 
 
-const Card = ({ image, className, alt, title, description }) => {
+const Card = ({ image, alt, title, description, category }) => {
   return (
     <div className='card'>
       <div className="card-content">
         <div className="card-text">
           <h4 className='card-title'>{title}</h4>
           <p className='card-description'>{description}</p>
+          <div className="tags">
+            {category.map((tag, index) => (
+              <span key={index} className="tag">{tag}</span>
+            ))}
+          </div>
         </div>
-          <img className="card-thumbnail" src={image} alt={alt}/>
+        <img className="card-thumbnail" src={image} alt={alt}/>
       </div>
     </div>
     
@@ -105,10 +128,10 @@ const Projects = () => {
         <Card 
             key={project.id} 
             image={project.image} 
-            class={project.class}
             alt={project.alt} 
             title={project.title} 
             description={project.description}
+            category={project.category}
         />
       ))}
     </div>
