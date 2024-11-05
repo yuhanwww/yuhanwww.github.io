4778d78fc62ecef462b7477bda871038ecf7e0b9
yuhanwww <ywang70@smith.edu>
2024-11-05 12:21:02 -0500

    Setup project list Card component

--- src/components/Project/projects.jsx
+++ src/components/Project/projects.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import './projects.css';
+import exploreCSR from '../../assets/projects/exploreCSR/exploreCSR.png';
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <div className="card-text">
+        <h4 className='card-title'>Header</h4>
+        <p className='card-description'>individual descriptionindividual descriptionindividual descriptionindividual descriptionindividual descriptionindividual descriptionindividual description</p>
+      </div>
+      <img className='card-img' src={exploreCSR} alt='img'/>
+        
+        
+    </div>
+    
+  )
+}
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <Card />
   )
 }
 
