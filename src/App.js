a4ef6436bf2e9e7e89bbf219cef190a9bb21880e
yuhanwww <ywang70@smith.edu>
2024-11-15 08:26:12 -0500

    still deploying

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Artwork from './components/Artwork/artwork';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <NavBar />
         <Routes>
           <Route exact path="/" element={<Intro />} />
