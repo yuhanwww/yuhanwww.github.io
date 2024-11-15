f202efebe5d817cb338e9c7f986896a48d6bf55c
yuhanwww <ywang70@smith.edu>
2024-11-15 08:16:14 -0500

    debug deploy

--- src/components/Artwork/artwork.jsx
+++ src/components/Artwork/artwork.jsx
@@ -6,7 +6,7 @@ const Artwork = () => {
   return (
     <section className="artwork section" id='artwork'>
         <h2 className="section_title">Artwork</h2>
-        <span className="section_subtitle">A selection of igital, Multi-media, and paper-based artworks.</span>
+        <span className="section_subtitle">A selection of digital, Multi-media, and paper-based artworks.</span>
         <div className="artwork container">
           <Artworks />
         </div>
