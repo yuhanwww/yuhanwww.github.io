580664c6fbdac6d8293d5792c6cd32da04ddaef9
yuhanwww <ywang70@smith.edu>
2024-12-04 18:32:28 -0500

    finish hci page

--- src/components/Project/Project_pages/hci.jsx
+++ src/components/Project/Project_pages/hci.jsx
@@ -3,10 +3,13 @@ import Iframe from 'react-iframe';
 
 import key_features from '../../../assets/projects/hci/key_features.png';
 import interfaces from '../../../assets/projects/hci/interfaces.png';
-import CS325_Team_JAYC_Poster_pdf from '../../../assets/files/hci/CS325_Team_JAYC_Poster.pdf';
 import wechat_prototype from '../../../assets/projects/hci/wechat_prototype.png';
 import wireframe from '../../../assets/projects/hci/wireframe.png';
 import prototype_qr from '../../../assets/projects/hci/prototype_qr.png';
+import paper_prototype from '../../../assets/projects/hci/paper_prototype.png';
+
+
+import CS325_Team_JAYC_Poster_pdf from '../../../assets/files/hci/CS325_Team_JAYC_Poster.pdf';
 import Milestone_1 from '../../../assets/files/hci/Milestone_1.pdf';
 import Milestone_2 from '../../../assets/files/hci/Milestone_2.pdf';
 import Milestone_3 from '../../../assets/files/hci/Milestone_3.pdf';
@@ -30,12 +33,11 @@ const HCI = () => {
           </p>
           <hr className="project-line" />
           <br/>
-          
+
+          {/* =========== Prototype ============*/}
           <div class="row">
             <div class="column">
               <img src={prototype_qr} alt="QR Code to Prototype" className="project_page-image-qr"/>
-            </div>
-            <div class="column">
               <p className="artwork_page-description">
                 <a target="_blank" rel="noopener noreferrer" 
                   href="https://www.figma.com/proto/whmyiA7yTiJVOMYtopcm5t/CS-325-High-fidel-Prototype?node-id=7-448&node-type=canvas&t=ey2DlQ8IW6x8TXiI-0&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=7%3A448">
@@ -44,8 +46,14 @@ const HCI = () => {
                 </a>
               </p>
             </div>
+            <div class="column">
+              <img src={interfaces} alt="QR Code to Prototype" className="project_page-image-"/>
+            </div>
           </div>
+        <br/>
         <hr className="project-line" />
+
+        {/* =========== Research ============*/}
         <br/>
         <p className="project-description">
           We started our project by surveying international students' need as well as conducting literature review on works to support internatioanl students in their foreign study. 
@@ -64,11 +72,22 @@ const HCI = () => {
           We targeted, from WeChat, things we want to include that is lacking: professional guidance on things like visa help & cross-cultural recognition and representation.
         </p>
         <hr className="project-line" />
+
+        {/* =========== Persona & Prototype ============*/}
         <br/>
         <p className="project-description">
           After qualitatively analyzing our survey result, we created <a target="_blank" rel="noopener noreferrer" href={Milestone_1}>2 personas</a> and <a target="_blank" rel="noopener noreferrer" href={Milestone_2}>4 tasks</a> to guide our prototype. 
           We initiated low-fidelity prototype for 3 modalities: App, Website, and Kiosk. 
           Each of them has its own strength, but based on our preliminary testing and insight from literature review, we decided to create the final prototype in App modality.
+        </p>
+        <img src={paper_prototype} alt="Paper Prototype of App" className="project_page-image"/>
+        <hr className="project-line" />
+        <br/>
+        <p className="project-description">
+          Informed by our low-fidelity prototype, we finally created our high-fidelity prototype in Figma. 
+          We started from setting up a universal color theme, icon set, fonts, and other design details to facilitate an internal consistency. 
+          Then we expanded our 4 main functions on 4 pages, guided by our tasks. 
+          We performed <a target="_blank" rel="noopener noreferrer" href={Milestone_3}>user testing</a> on the final prototype and made improvements accordingly.
         </p>
 
       </span>
