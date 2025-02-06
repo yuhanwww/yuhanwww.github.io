4d0f3672c4a6405a98f2e2bbe1b83f6c953a2d7e
yuhanwww <ywang70@smith.edu>
2025-02-05 22:28:04 -0500

    Update footer.jsx

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   const [year, setYear] = useState(new Date().getFullYear());
     return (
         <footer className="footer">
-            &#169; {year} Yuhanwww | Poweverd by &#x2661; from Decora D
+            &#169; {year} Yuhanwww | Poweverd by &#x2661; from <a target="_blank" rel="noopener noreferrer" href="https://www.smith.edu/academics/design-thinking">&nbsp;DTI</a>
         </footer>
     )
 };
