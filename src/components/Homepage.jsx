aae8a89f044f75b43a710c9f774fef141ed2d2cc
yuhanwww <ywang70@smith.edu>
2026-02-05 21:10:01 -0500

    change miscellaneous

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -4,6 +4,7 @@ import NavBar from './NavBar/navbar';
 import Intro from "./Intro/intro";
 import Project from './Project/project';
 import Artwork from './Artwork/artwork';
+import Publication from './Publication/publication';
 import Footer from './Footer/footer';
 
 const Homepage = () => {
@@ -11,6 +12,7 @@ const Homepage = () => {
     <div className="app-container">
        {/* <NavBar /> */}
         <Intro />
+        <Publication />
         <Project />
         <Artwork />
        {/* <Footer /> */}
